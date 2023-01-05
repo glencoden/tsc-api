3dac908f1a206fec4fbde3cd2991bebcbd499592
glen coden <simon.der.meyer@gmail.com>
2023-01-05 14:03:11 +0100

    tsc npm script

--- dist/features/api/helpers/isObject.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isObject = void 0;
-function isObject(val) {
-    return val !== null && typeof val !== 'function' && typeof val === 'object' && !Array.isArray(val);
-}
-exports.isObject = isObject;
-//# sourceMappingURL=isObject.js.map
